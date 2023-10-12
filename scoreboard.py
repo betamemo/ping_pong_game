2bcae6e8c2ac956ea53e538baf9d04b8e726a64b
BK <betamemo@BK.local>
2023-10-12 12:03:22 +0700

    print score

---
+++ scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.left_score = 0
+        self.right_score = 0
+        self.print_score()
+
+    def print_score(self):
+        self.goto(0, 200)
+        self.write(f'{self.left_score}\t:\t{self.right_score}', align='center', font=('Arial', 50, 'normal'))
+
+    def update_left(self):
+        self.left_score += 10
+        self.clear()
+        self.print_score()
+
+    def update_right(self):
+        self.right_score += 10
+        self.clear()
+        self.print_score()
